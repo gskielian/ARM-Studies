f697de9dd80d2d233e404e03967b0150d423bea1
Gregory S. Kielian <gregory.kielian@gmail.com>
2016-06-07 21:43:05 -0700

    HelloWorld added functions and Makefile objectdeps
    Blinking light with variable delay implemented

--- STM32-Template/Hello_World/main.c
+++ STM32-Template/Hello_World/main.c
@@ -10,27 +10,53 @@ int main(void) {
 
 	//Enable Peripheral Clocks
 
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE); 
+
 	//Configure pins
 
+	GPIO_StructInit(&GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+
 	//Configure Systick Timer
+
+
+	if (SysTick_Config(SystemCoreClock/1000))
+		while(1);
 
 
 	while (1) {
 		static int ledval = 0;
 
 		// toggle led
+		GPIO_WriteBit(GPIOC, GPIO_Pin_9, (ledval) ? Bit_SET : Bit_RESET);
+		ledval = 1 - ledval;
 
-		Delay(250);
+		Delay(10);
 	}
 }
 
 //Timer Code
+static __IO uint32_t TimingDelay;
+
+void Delay(uint32_t nTime) {
+	TimingDelay = nTime;
+	while(TimingDelay != 0);
+}
+
+
+void SysTick_Handler(void) {
+	if (TimingDelay != 0x00)
+		TimingDelay--;
+}
 
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t* file, uint32_t line) {
-/*infinite loop*/
-/* Use gdbb to find out why we're here */
+	/*infinite loop*/
+	/* Use gdbb to find out why we're here */
 
-while (1);
+	while (1);
 }
 #endif
